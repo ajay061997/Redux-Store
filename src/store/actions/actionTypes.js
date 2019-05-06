ac3db417a3fad1417b22a0208b7c19802005ef47
Ajay <35392022+ajay061997@users.noreply.github.com>
2019-05-06 13:16:50 +0530

    added comment

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -5,51 +5,51 @@ export const REMOVE5 = 'REMOVE5';
 export const ADD_DATA = 'ADD_DATA';
 export const REMOVE_DATA = 'REMOVE_DATA';
 
-export const increment = () => {
-    return {
-        type: INCREMENT
-    }
-}
+// export const increment = () => {
+//     return {
+//         type: INCREMENT
+//     }
+// }
 
-export const decrement = () => {
-    return {
-        type: DECREMENT
-    }
-}
+// export const decrement = () => {
+//     return {
+//         type: DECREMENT
+//     }
+// }
 
-export const add5 = (val) => {
-    return {
-        type: ADD5,
-        value: val
-    }
-}
+// export const add5 = (val) => {
+//     return {
+//         type: ADD5,
+//         value: val
+//     }
+// }
 
-export const remove5 = (val) => {
-    return {
-        type: REMOVE5,
-        value: val
-    }
-}
+// export const remove5 = (val) => {
+//     return {
+//         type: REMOVE5,
+//         value: val
+//     }
+// }
 
-export const saveResult = (res) => {
-    return {
-        type: ADD_DATA,
-        result: res
-    }
-}
+// export const saveResult = (res) => {
+//     return {
+//         type: ADD_DATA,
+//         result: res
+//     }
+// }
 
-export const addData = (res) => {
-    return (dispath) => {
-        setTimeout(() => {
-            dispath(saveResult(res))
-        }, 2000)
-    }
-}
+// export const addData = (res) => {
+//     return (dispath) => {
+//         setTimeout(() => {
+//             dispath(saveResult(res))
+//         }, 2000)
+//     }
+// }
 
-export const removeData = (id) => {
-    return {
-        type: REMOVE_DATA,
-        resulElId: id
-    }
-}
+// export const removeData = (id) => {
+//     return {
+//         type: REMOVE_DATA,
+//         resulElId: id
+//     }
+// }
 
